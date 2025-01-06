c39b14895935ff6c5e2bce0a03bf4e3bcdf3e9e9
Alex-can-dev <alex@albioza.pl>
2025-01-06 20:06:32 +0100

    Add count_charakters function

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ def count_words(file):
     count_of_words = len(words)
     return count_of_words
 
+# return the number of times each character appears in the book
+def count_charakters(file):
+    lowered_words = file.lower()
+    list = {}
+
+    for char in lowered_words:
+        if char.isalpha() == True:
+            if char in list:
+                list[char] += 1
+            else:
+                list[char] = 1
+
+    return list
+
 def main():
     path_to_file = "books/frankenstein.txt"
 
@@ -11,5 +25,6 @@ def main():
         file_contents = f.read()
 
     print(count_words(file_contents))
+    print(count_charakters(file_contents))
 
 main()
