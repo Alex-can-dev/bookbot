e8427b0892140c6c373f1951df93d9a056f9f23a
Alex-can-dev <alex@albioza.pl>
2025-01-07 21:22:11 +0100

    Print formatted report

--- main.py
+++ main.py
@@ -7,24 +7,34 @@ def count_words(file):
 # return the number of times each character appears in the book
 def count_charakters(file):
     lowered_words = file.lower()
-    list = {}
+    all_char = {}
 
     for char in lowered_words:
         if char.isalpha() == True:
-            if char in list:
-                list[char] += 1
+            if char in all_char:
+                all_char[char] += 1
             else:
-                list[char] = 1
+                all_char[char] = 1
 
-    return list
+    my_keys = list(all_char.keys())
+    my_keys.sort()
+
+    sd = {i: all_char[i] for i in my_keys}
+
+    return sd
 
 def main():
     path_to_file = "books/frankenstein.txt"
+    print("--- Begin report of " + path_to_file + " ---")
 
     with open(path_to_file) as f:
         file_contents = f.read()
 
-    print(count_words(file_contents))
-    print(count_charakters(file_contents))
+    print(str(count_words(file_contents)) + " words found in the document\n")
+    
+    for key, value in count_charakters(file_contents).items():
+        print(f"The '{key}' character was found {value} times")
+    
+    print("--- End report ---")
 
 main()
